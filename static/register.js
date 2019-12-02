65d986d9c7acd99f1e332d6366f004ad1daa3149
Sajan491 <sajanmahat491@gmail.com>
2019-12-02 13:21:03 +0545

    corrected errors

--- static/register.js
+++ static/register.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     var a = document.querySelectorAll('li')
 
-    for (i = 0; i < a.length; i++) {
+    for (i = 0; i < a.length - 4; i++) {
         a[i].style.display = 'none'
     }
 
     var b = document.querySelectorAll('span')
 
-    for (i = 0; i < b.length; i++) {
+    for (i = 0; i < b.length - 3; i++) {
         b[i].textContent = ''
     }
 
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     var inputs = document.querySelectorAll('input')
-    var placeholder = ['', 'Enter Your Username', 'Enter Your Email', 'Enter Your Password', 'Enter Your Password Again']
+    var placeholder = ['', 'Enter Your Username', 'Enter Your Email', 'Enter Your Password', 'Enter Your Password Again', 'Search']
     for (i = 1; i < inputs.length; i++) {
         inputs[i].placeholder = placeholder[i]
     }
