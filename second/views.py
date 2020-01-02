762c428ee1f222521c69f9958360f392c561837e
nripesh <nripeshk8@gmail.com>
2020-01-02 14:30:25 +0545

    UserPassesTestMixin for creating post

--- second/views.py
+++ second/views.py
@@ -39,7 +39,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return False
 
 
-class PostCreateView(LoginRequiredMixin, CreateView):
+class PostCreateView(LoginRequiredMixin,UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'content']
     template_name = 'post_form.html'
@@ -47,6 +47,11 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        if self.request.user.last_name == 'teacher':
+            return True
+        return False
 
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -73,8 +78,8 @@ def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
+            request.FILES,
+            instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -82,8 +87,8 @@ def profile(request):
             return redirect('profile')
     
     else:
-         u_form = UserUpdateForm(instance=request.user)
-         p_form = ProfileUpdateForm(instance=request.user.profile)
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
