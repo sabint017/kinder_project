d37c84fa534ce6de8816ce67ac9f5c88da98c8af
Sabin Thapa <sabint017@gmail.com>
2020-01-02 10:13:14 +0545

    Message

--- kinder/urls.py
+++ kinder/urls.py
@@ -5,6 +5,9 @@ from users import views as users_views
 from django.contrib.auth import views as auth_views
 
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('first.urls')),
@@ -16,3 +19,6 @@ urlpatterns = [
 
     # path('profile/', users_views.profile, name = 'profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- second/apps.py
+++ second/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class SecondConfig(AppConfig):
     name = 'second'
+
+    def ready(self):
+        import second.signals
