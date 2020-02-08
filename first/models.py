327c83fb29da8d71c83d99d0058d85abc15b874c
Sabin Thapa <sabint017@gmail.com>
2020-02-08 19:17:21 +0545

    contact form updated

--- first/models.py
+++ first/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
     email = models.EmailField()
     message = models.TextField()
