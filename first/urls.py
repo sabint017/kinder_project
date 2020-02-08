833c6527dd68267dd8407aac8832d42f4e8eb21f
sajan491 <sajanmahat491@gmail.com>
2020-02-08 15:17:13 +0545

    index page animations

--- first/urls.py
+++ first/urls.py
@@ -6,14 +6,13 @@ from second import views as second_views
 urlpatterns = [
     path('', views.index, name='index'),
 
-    path('home.html',second_views.home, name='home'),
+    path('home.html', second_views.home, name='home'),
     path('dev.html', views.dev, name='dev'),
     path('queries.html', views.queries, name='queries'),
     path('services.html', views.services, name='services'),
     path('register_parent.html', users_views.register_parent, name="register_parent"),
     path('register_teacher.html', users_views.register_teacher, name="register_teacher"),
     path('signup.html', views.signup, name='signup'),
-    path('contact.html', views.contact, name='contact'),
-   
+
 
 ]
--- first/views.py
+++ first/views.py
@@ -3,8 +3,40 @@ from django.template.loader import get_template
 from .forms import ContactForm
 from django.core.mail import EmailMessage
 
+
 def index(request):
-    return render(request, 'index.html')
+    Contact_Form = ContactForm
+    if request.method == 'POST':
+        form = Contact_Form(data=request.POST)
+
+        if form.is_valid():
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
+
+            template = get_template('contact_form.txt')
+            context = {
+                'first_name': first_name,
+                'lastname': last_name,
+                'email': email,
+                'message': message,
+            }
+
+            content = template.render(context)
+
+            email = EmailMessage(
+                "Contact Email",
+                content,
+                "Kinder Mail" + '',
+                ['nripeshk8@gmail.com'], ['sajanmahat491@gmail.com'],
+                headers={'Reply To': email}
+            )
+
+            email.send()
+
+            return render(request, 'succ.html')
+    return render(request, 'index.html', {'form': Contact_Form})
 
 
 def queries(request):
@@ -18,44 +50,9 @@ def dev(request):
 def services(request):
     return render(request, 'services.html')
 
+
 def signup(request):
     return render(request, 'signup.html')
 
 
-
-
 # Create your views here.
-
-def contact(request):
-    Contact_Form = ContactForm
-    if request.method == 'POST':
-        form = Contact_Form(data=request.POST)
-
-        if form.is_valid():
-            first_name = request.POST.get('first_name')
-            last_name = request.POST.get('last_name')
-            email = request.POST.get('email')
-            message = request.POST.get('message')
-
-            template = get_template('contact_form.txt')
-            context = {
-                'first_name' : first_name,
-                'lastname' : last_name,
-                'email' : email,
-                'message' : message,
-            }
-
-            content = template.render(context)
-
-            email = EmailMessage(
-                "Contact Email",
-                content,
-                "Kinder Mail" + '',
-                ['nripeshk8@gmail.com'],['sajanmahat491@gmail.com'],
-                headers = { 'Reply To': email }
-            )
-
-            email.send()
-
-            return render(request, 'succ.html')
-    return render(request, 'index.html', {'form':Contact_Form })
