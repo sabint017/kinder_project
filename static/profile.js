d2825dd6fb357d93aae8d68328275ecb8880abd1
sajan491 <sajanmahat491@gmail.com>
2020-03-09 12:13:35 +0545

    profile page

---
+++ static/profile.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+
+
+    var labels = document.querySelectorAll('label')
+    for (i = 0; i < labels.length; i++) {
+        labels[i].style.display = 'none'
+    }
+
+
+
+})
