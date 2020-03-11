1ada3914d5b8df16ea6c697a38101f3c5cff2af3
Sabin Thapa <sabint017@gmail.com>
2020-03-11 17:07:34 +0545

    fixed contact form bug

--- first/views.py
+++ first/views.py
@@ -10,13 +10,13 @@ def index(request):
         form = Contact_Form(data=request.POST)
 
         if form.is_valid():
-            name = request.POST.get('last_name')
+            name = request.POST.get('name')
             email = request.POST.get('email')
             message = request.POST.get('message')
 
             template = get_template('contact_form.txt')
             context = {
-                'name': last_name,
+                'name': name,
                 'email': email,
                 'message': message,
             }
@@ -27,7 +27,7 @@ def index(request):
                 "Contact Email",
                 content,
                 "Kinder Mail" + '',
-                ['nripeshk8@gmail.com'], ['sajanmahat491@gmail.com'],
+                ['sabint017@gmail.com'], ['sajanmahat491@gmail.com'],
                 headers={'Reply To': email}
             )
 
@@ -59,15 +59,13 @@ def contact(request):
         form = Contact_Form(data=request.POST)
 
         if form.is_valid():
-            first_name = request.POST.get('first_name')
-            last_name = request.POST.get('last_name')
+            name = request.POST.get('name')
             email = request.POST.get('email')
             message = request.POST.get('message')
 
             template = get_template('contact_form.txt')
             context = {
-                'first_name': first_name,
-                'lastname': last_name,
+                'name': name,
                 'email': email,
                 'message': message,
             }
@@ -78,7 +76,7 @@ def contact(request):
                 "Contact Email",
                 content,
                 "Kinder Mail" + '',
-                ['nripeshk8@gmail.com'], ['sajanmahat491@gmail.com'],
+                ['sabint017@gmail.com'], ['sajanmahat491@gmail.com'],
                 headers={'Reply To': email}
             )
 
